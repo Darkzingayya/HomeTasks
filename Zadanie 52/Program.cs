1288e2d34cbbee00116fcb9e791d87d9ccd9cb0e
Alexey_Strelnikov <darkzingayya@gmail.com>
2023-01-02 09:57:49 +0300

    02,01,DZ

--- Zadanie 52/Program.cs
+++ Zadanie 52/Program.cs
@@ -15,10 +15,23 @@ void InputMatrix(int[,] matrix)
 }
 
 
+void MidlArf(int[,] matrix)
+{ 
+   for (int j = 0; j < matrix.GetLength(1); j++)
+      { double midl = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+         midl += matrix[i, j];
+        }
+         Console.Write($"{midl/matrix.GetLength(0)} \t");
+   
+      }
+}
+
 
 Console.Clear();
 Console.Write("Введите размер матрицы: ");
 string[] numbers = Console.ReadLine().Split(" ");
 int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
 InputMatrix(matrix);
-
+MidlArf(matrix);
