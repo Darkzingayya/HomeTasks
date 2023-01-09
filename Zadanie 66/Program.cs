21737045adca947239d8db5682d21a0a8c85cc30
Alexey_Strelnikov <darkzingayya@gmail.com>
2023-01-09 04:34:10 +0300

    09,01,DZ

---
+++ Zadanie 66/Program.cs
@@ -0,0 +1,22 @@
+﻿// 66 Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int Summ(int n, int m, int sum)
+{
+   sum += n;
+   if (n == m)
+      return sum;
+   return Summ ( n + 1, m, sum);
+}
+
+Console.Clear();
+int sum = 0;
+Console.WriteLine("Введите начало интервала: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конец интервала: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n < m && 0 <n)
+Console.WriteLine(Summ(n, m, sum));
+else
+Console.WriteLine("Не корректный ввод!");
---
+++ Zadanie 68/Program.cs
@@ -0,0 +1,24 @@
+﻿// 68 Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Akk(int m, int n)
+{
+   if (m == 0)
+    return n + 1;
+
+      else if (m > 0 && n == 0)
+       return Akk(m-1, 1);
+
+         else if (m > 0 && n > 0)
+         return Akk (m - 1, Akk(m, n - 1));
+   return m; // Без этого Ретурн появляется ошибка "не все пути к коду возвращают значение."
+}
+
+Console.Clear();
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akk(m, n));
+
+// В итоге как то работает и ответы совпадают, но всё равно не понимаю этого Аккермана))
